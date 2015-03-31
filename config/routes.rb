a8fb3daaf4f896c88d615d9a8ca1c2395357e281
Aaron Bao <aaronb0608@gmail.com>
2015-03-31 11:53:22 -0500

    done up to part3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :customers
-
+  resources :finder
+  
+  get 'missing_email'  =>  'finder#missing_email',  as: 'missing_email'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
